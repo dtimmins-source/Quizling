ab6aa6220d4064af7b3ebeb3a79f7c5cf3d9afd1
Dan Timmins <dantimmins97@gmail.com>
2024-03-31 14:16:40 -0400

    Wrong answers now have a message and indicate the correct answer.

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -9,6 +9,9 @@ public class Quiz : MonoBehaviour
     [SerializeField]TextMeshProUGUI questionText;
     [SerializeField] QuestionSO question;
     [SerializeField] GameObject[] answerButtons;
+    int correctAnswerIndex;
+    [SerializeField] Sprite defaultAnswerSprite;
+    [SerializeField] Sprite correctAnswerSprite;
     
     void Start()
     {
@@ -20,5 +23,21 @@ public class Quiz : MonoBehaviour
 
             buttonText.text = question.GetAnswer(i);
         }   
+    }
+
+    public void OnAnswerSelected(int index)
+    {
+            correctAnswerIndex = question.GetCorrectAnswerIndex();
+            if(index == correctAnswerIndex)
+            {
+                questionText.text = "Correct!";
+            }
+            else
+            {
+                string correctAnswer = question.GetAnswer(correctAnswerIndex);
+                questionText.text = "Incorrect! The correct answer is:\n" + correctAnswer;
+            }
+            Image buttonImage = answerButtons[correctAnswerIndex].GetComponent<Image>();
+            buttonImage.sprite = correctAnswerSprite;
     }
 }
